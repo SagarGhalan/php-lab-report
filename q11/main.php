11c3848304493dc2f9273868222d95be5ce034b1
Sagar Ghalan <99875649+4nwnone@users.noreply.github.com>
2022-05-05 20:49:34 +0545

    Update main.php

--- q11/main.php
+++ q11/main.php
@@ -4,7 +4,7 @@ if(!isset($_POST['submit']))
 {
     session_start();
     $_SESSION['chance']=2;
-    header ('location:q11.php');
+    header ('location:form.php');
 
 }
 if(isset($_POST['submit']))
@@ -20,11 +20,11 @@ if(isset($_POST['submit']))
         $pass = $_POST['pass'];
 
         if($uname == $username && $pass == $password){
-            header('location:form.php');
+            header('location:sucessform.php');
        }
        else{
         $_SESSION['chance']--;
-        header('location:successform.php?error=1');
+        header('location:form.php?error=1');
         }
     }
     else{
